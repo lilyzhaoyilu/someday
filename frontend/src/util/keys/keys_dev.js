b86671ebea45e14ffd43c1a57ea76f27d48bb301
Michael Chen <atlasneiko@gmail.com>
2020-12-28 10:15:15 -0800

    key security

--- frontend/src/util/keys/keys_dev.js
+++ frontend/src/util/keys/keys_dev.js
@@ -1,1 +1,2 @@
 export default devKey;
+const devKey = '';
--- frontend/src/util/movie_util.js
+++ frontend/src/util/movie_util.js
@@ -1,15 +1,6 @@
 import axios from 'axios';
 
-//? movieData reference
-//* var options = {
-//*   method: 'GET',
-//*   url: 'https://imdb8.p.rapidapi.com/title/get-details',
-//*   params: { tconst: 'tt0944947' },
-//*   headers: {
-//*     'x-rapidapi-key': '4f6597c142mshbbf844493d34de0p191b12jsna11cfd54f891',
-//*     'x-rapidapi-host': 'imdb8.p.rapidapi.com'
-//*   }
-//* };
+
 
 export const getThisMovie = (movieData) => (
   axios.request(movieData)
