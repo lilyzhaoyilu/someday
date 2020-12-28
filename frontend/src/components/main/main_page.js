513db96d26a91574d27f508bf6ecf5a432ee5d74
Michael Chen <atlasneiko@gmail.com>
2020-12-28 07:14:08 -0800

    set up frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>Someday</h1>
+    <footer>
+      {`copyright &copy; 2020 Someday`}
+    </footer>
+  </div>
+)
---
+++ frontend/src/components/nav/navbar_container.jsx
@@ -0,0 +1,8 @@
+import { connect } from "react-redux";
+import { logout } from "../../actions/session_actions";
+import NavBar from "./navbar";
+
+const mapStateToProps = (state) => ({
+	loggedIn: state.session.isAuthenticated,
+});
+export default connect(mapStateToProps, { logout })(NavBar);
