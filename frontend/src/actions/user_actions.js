f33f839ccc14cde3752fdf090c221676710688cb
Lily Lu <lilyzhaoyilu@gmail.com>
2020-12-29 13:52:48 -0800

    Update user_actions.js

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -4,7 +4,8 @@ import { receiveCurrentUser } from './session_actions'
 export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS"
 export const FETCH_THIS_USER = "FETCH_THIS_USER";
 export const FETCH_ALL_USER = "FETCH_ALL_USER";
-
+export const RECEIVE_THIS_USER = "RECEIVE_THIS_USER";
+export const RECEIVE_ALL_USER = "RECEIVE_ALL_USER";
 
 const receiveErrors = errors => ({
   type: RECEIVE_USER_ERRORS,
