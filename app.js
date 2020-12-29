d0fe0dbe60ad13716b52637d8f34d7c8445d3ad5
Lily Lu <lilyzhaoyilu@gmail.com>
2020-12-29 14:16:14 -0800

    update for deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const db = require('./config/keys').mongoURI;
 
-app.get("/", (req, res) => res.send("Hello World"));
+// app.get("/", (req, res) => res.send("Hello World"));
 const users = require("./routes/api/users");
 
 mongoose
@@ -17,6 +17,11 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+
+
+
+
+const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/public'));
