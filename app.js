81dc95de67b2940ff3e79868280bf26e606f869a
Michael Chen <atlasneiko@gmail.com>
2020-12-29 13:34:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,11 @@ app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/public'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+  })
+}
