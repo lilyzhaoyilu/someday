3a107f1f295b9d17fa0c044748d24807c38eab4b
Lily Lu <lilyzhaoyilu@gmail.com>
2020-12-30 09:21:35 -0800

    update on navbar

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,23 +3,21 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util'
 import { Switch, Route } from 'react-router-dom';
 
-import NavBarContainer from './nav/navbar_container'
+// import NavBarContainer from './nav/navbar_container'
 
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
-import MovieIndexContainer from './movie/movie_index_container'
-import SplashContainer from './splash/splash_container'
+import SplashContainer from './splash/splash_container';
 
 const App = () => (
   <div>
-    <NavBarContainer />
+  
     <Switch>
       <AuthRoute exact path='/login' component={LoginFormContainer} />
       <AuthRoute exact path='/signup' component={SignupFormContainer} />
-      <ProtectedRoute exact path='/' component={MainPage} />
-      <AuthRoute path='/splash' component={SplashContainer} />
-
+      <AuthRoute exact path='/splash' component={SplashContainer} />
+      <ProtectedRoute  path='/' component={MainPage} />
     </Switch>
   </div>
 )
