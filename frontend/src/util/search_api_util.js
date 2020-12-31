276b3ec968c208d9868b5e2dc1defc43fd3f169a
Michael Chen <atlasneiko@gmail.com>
2020-12-31 05:55:55 -0800

    Update search_api_util.js

--- frontend/src/util/search_api_util.js
+++ frontend/src/util/search_api_util.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 
 //* get /api/search/list search watchedlist/todolit with name
 
-export const searchLists = (listName) => (
-  axios.get('/api/search/list', listName)
+export const searchLists = (list) => (
+  axios.get('/api/search/search/list', list)
 )
 
-//* get /api/search/movie search movies with name
+// //* get /api/search/movie search movies with name
 
-export const searchMovies = searchResult => (
-  axios.get('/api/search/movie', searchResult)
-)
+// export const searchMovies = searchResult => (
+//   axios.get('/api/search/movie', searchResult)
+// )
 
 //* get /api/search/handle search users with handle
 
-export const searchUsers = userHandle => (
-  axios.get('/api/search/handle', userHandle)
+export const searchUsers = handle => (
+  axios.get('/api/search/search/handle', handle)
 )
