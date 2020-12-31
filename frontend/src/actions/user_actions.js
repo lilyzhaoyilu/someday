a82c3e2cd0a49278eedb66f3a2bf9adda1ed90f6
Michael Chen <atlasneiko@gmail.com>
2020-12-31 08:02:52 -0800

    add searchUser under user_actions

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,7 +1,9 @@
 import * as USERAPI from '../util/user_api_util'
+import * as SEARCHAPI from '../util/search_api_util'
 import { receiveCurrentUser } from './session_actions'
 
 export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS"
+export const FETCH_USERS = 'RECEIVE_USERS'
 export const FETCH_THIS_USER = "FETCH_THIS_USER";
 export const FETCH_ALL_USER = "FETCH_ALL_USER";
 
@@ -14,6 +16,11 @@ const receiveErrors = errors => ({
 const receiveThisUser = user => ({
   type: FETCH_THIS_USER,
   user
+})
+
+const receiveUsers = users => ({
+  type: FETCH_USERS,
+  users
 })
 
 const receiveAllUsers = users => ({
@@ -42,6 +49,14 @@ export const fetchThisUser = userId => dispatch => (
   USERAPI.getThisUser(userId)
     .then(
       user => dispatch(receiveThisUser(user)),
+      err => dispatch(receiveErrors(err))
+    )
+)
+
+export const searchUsers = handle => dispatch => (
+  SEARCHAPI.searchUsers(handle)
+    .then(
+      users => dispatch(receiveUsers(users)),
       err => dispatch(receiveErrors(err))
     )
 )
