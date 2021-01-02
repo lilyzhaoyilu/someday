10895d57654afe4387cea8c91d6b99d020de23f5
Lily Lu <lilyzhaoyilu@gmail.com>
2021-01-01 16:43:09 -0800

    update app js, adding useUnifiedTopology

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const search = require("./routes/api/search");
 const comments = require("./routes/api/comments");
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true , useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
