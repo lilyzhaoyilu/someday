1f75c2dd0fb7f7a6271f0ce035a2b6a7c95a752c
Rex Gao <rexgao2013@gmail.com>
2021-01-03 11:53:34 -0800

    "add loading phase to media page"

---
+++ frontend/src/components/media_page/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+  <div className= "loading-wrap">
+    <svg className= "loading-body">
+        <circle cx='70' cy='70' r='70' className= "circle"></circle>
+    </svg>
+  </div>
+);
+
+
