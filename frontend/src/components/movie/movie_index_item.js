e687038d8fbf160dbf7256d1ee55d62ac62e7616
Michael Chen <atlasneiko@gmail.com>
2021-01-14 10:13:32 -0800

    fix prelogin media page

--- frontend/src/components/movie/movie_index_item.js
+++ frontend/src/components/movie/movie_index_item.js
@@ -3,19 +3,27 @@ import { Link } from 'react-router-dom'
 
 export default class MovieIndexItem extends Component {
   render() {
-    const { movie, imgUrl, name, year } = this.props
-    return (
+    console.log(this.props);
+    const { movie, imgUrl, name, year, loggedIn } = this.props
+    return loggedIn ? (
       <Link to={`/mediaPage/${movie.id}`} className="tooltip search-result-item" >
-      
-        
         <span className="tooltiptext">{name}
-        <br></br>{year}</span>
+          <br></br>{year}</span>
         <div>
-            <img src={imgUrl} alt={name} style={{ width: '200px', height: '300px' }} />    
-     
-      </div>
+          <img src={imgUrl} alt={name} style={{ width: '200px', height: '300px' }} />
+
+        </div>
       </Link>
-    )
+    ) : (
+        <Link to={`/media_page/${movie.id}`} className="tooltip search-result-item" >
+          <span className="tooltiptext">{name}
+            <br></br>{year}</span>
+          <div>
+            <img src={imgUrl} alt={name} style={{ width: '200px', height: '300px' }} />
+
+          </div>
+        </Link>
+      )
   }
 }
 
