170d348969d5a048feffbe506a5d7e9f473d81f4
Sam Wong <swonggzz@gmail.com>
2021-01-15 20:20:02 -0800

    added metadata for linkedin featured

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -14,7 +14,19 @@ import SplashContainer from './splash/splash_container';
 import MoviePageContainer from './splash/splash_media/media_page_cotainer';
 const App = () => (
   <div>
-
+    <meta property="og:title" content="Someday" />
+    <meta property='og:image' content="https://github.com/lilyzhaoyilu/someday/blob/main/frontend/public/splash.png"/>
+    <meta property="og:description"
+      content="Someday is a collaborative MERN project, using MongoDB, 
+      Express, React, and NodeJS. It helps users to keep track of the movies 
+      and TV shows that they have watched and want to watch. This web application 
+      that allows users to keep track of the shows and movies that they want to watch 
+      some day. Users can create lists to keep track of the shows and movies, leave comments 
+      to them, and look at other uses comments and lists."
+    />
+    <meta property="og:url"
+      content="https://somedaymern.herokuapp.com/#/splash"
+    />
     <Switch>
       <AuthRoute exact path='/splash' component={SplashContainer} />
       <AuthRoute exact path='/login' component={LoginFormContainer} />
