ba6571870cb543cf3bda3c60f7410cf28c210223
Lily Lu <lilyzhaoyilu@gmail.com>
2021-01-22 10:36:48 -0800

    update word style

--- frontend/src/components/media_page/userAlsoAdded/userAlsoAdded.jsx
+++ frontend/src/components/media_page/userAlsoAdded/userAlsoAdded.jsx
@@ -6,45 +6,45 @@ import { Link } from 'react-router-dom';
 
 class UserAlsoAdded extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.displayUsersAlsoAdded = this.displayUsersAlsoAdded.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.getHistorylistsBasedOnMovieId(this.props.movieId);
     this.props.getWatchlistsBasedOnMovieId(this.props.movieId);
   }
 
-  displayUsersAlsoAdded(){
-    if(this.props.usersAlsoAddedArray){
+  displayUsersAlsoAdded() {
+    if (this.props.usersAlsoAddedArray) {
       let array = this.props.usersAlsoAddedArray;
-      
-      return(
-        
-          array.map((list) => {
-            let listOnwerId = list.user;
-            let listOnwer;
-            if(this.props.users[listOnwerId]){
-              listOnwer = this.props.users[listOnwerId].handle
-            }else{
-              this.props.fetchThisUser(listOnwerId).then(()=>(listOnwer=this.props.users[listOnwerId].handle ))
-            }
 
-            return(<li>{listOnwer}added {this.props.movieTitle} to {(list.name)}</li>)
-          })
+      return (
+
+        array.map((list) => {
+          let listOnwerId = list.user;
+          let listOnwer;
+          if (this.props.users[listOnwerId]) {
+            listOnwer = this.props.users[listOnwerId].handle
+          } else {
+            this.props.fetchThisUser(listOnwerId).then(() => (listOnwer = this.props.users[listOnwerId].handle))
+          }
+
+          return (<li>{listOnwer} added {this.props.movieTitle} to {(list.name)}</li>)
+        })
       )
     }
   }
 
 
   render() {
-    
-    return(<div className="media-page-user-also-added">
+
+    return (<div className="media-page-user-also-added">
 
       They also added {this.props.movieTitle} to their lists {this.displayUsersAlsoAdded()}
-      
-      </div>)
+
+    </div>)
   }
 
 }
