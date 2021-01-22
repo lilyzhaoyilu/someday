1f2e21f16bf72c47d9e398ccd8602651e8908886
Michael Chen <atlasneiko@gmail.com>
2021-01-21 18:37:56 -0800

    add most comment users, newest users CSS style

--- frontend/src/components/main/new_user_index/new_user_item.jsx
+++ frontend/src/components/main/new_user_index/new_user_item.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
+import moment from "moment";
 
 export default ({ user, idx }) => {
-	let date = user.date.slice(0, 10).split("-");
-	[date[0], date[1], date[2]] = [date[1], date[2], date[0]];
-	date = date.join("/");
+	let date = moment(user.date).format("MMM D YYYY");
+
 	return (
 		<Link
 			to={`/profile/${user._id}`}
 			key={`new-user-item-${user._id}-${idx}`}
 			className="new-user-item"
 		>
+			{/* {console.log(date)} */}
 			<p>
 				<span className="user-item-username">{user.handle}</span> joined Someday
-				at ${date}
+				<br />
+				at {date}
 			</p>
 		</Link>
 	);
