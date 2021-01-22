81358b9cc2bd63c702f51e457fe288071a4e28a5
Rex Gao <rexgao2013@gmail.com>
2021-01-22 10:41:47 -0800

    fix the navbar display width, and show to tv show

--- frontend/src/components/movie/movie_fire.jsx
+++ frontend/src/components/movie/movie_fire.jsx
@@ -3,21 +3,21 @@ import React from 'react';
 function Fire (){
 
     return (
-      <div class="fire">
-        <div class="fire-left">
-          <div class="main-fire"></div>
-          <div class="particle-fire"></div>
+      <div className="fire">
+        <div className="fire-left">
+          <div className="main-fire"></div>
+          <div className="particle-fire"></div>
         </div>
-        <div class="fire-main">
-          <div class="main-fire"></div>
-          <div class="particle-fire"></div>
+        <div className="fire-main">
+          <div className="main-fire"></div>
+          <div className="particle-fire"></div>
         </div>
-        <div class="fire-right">
-          <div class="main-fire"></div>
-          <div class="particle-fire"></div>
+        <div className="fire-right">
+          <div className="main-fire"></div>
+          <div className="particle-fire"></div>
         </div>
-        <div class="fire-bottom">
-          <div class="main-fire"></div>
+        <div className="fire-bottom">
+          <div className="main-fire"></div>
         </div>
       </div>
     );
